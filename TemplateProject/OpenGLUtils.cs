8c5a46b7ad790f17a41038e1369e572b8126c35e
Tomasz Herman <tomasz.herman@protonmail.com>
2024-03-30 23:05:53 +0100

    check error func

---
+++ TemplateProject/OpenGLUtils.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using OpenTK.Graphics.OpenGL;
+
+namespace TemplateProject;
+
+public static class OpenGLUtils
+{
+    [Conditional("DEBUG")]
+    public static void CheckError()
+    {
+        ErrorCode error;
+        while ((error = GL.GetError()) != ErrorCode.NoError)
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+
+            Debug.Print($"Error: {error.ToString()}({(int) error})");
+        }
+    }
+}
