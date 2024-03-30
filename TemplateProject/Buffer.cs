f8f473a11b3d42fc6cc0534a247039e60bfb7f29
Tomasz Herman <tomasz.herman@protonmail.com>
2024-03-30 20:54:14 +0100

    Moved buffer related classes to Buffer.cs

---
+++ TemplateProject/Buffer.cs
@@ -0,0 +1,236 @@
+using System.Runtime.InteropServices;
+using OpenTK.Graphics.OpenGL4;
+
+namespace TemplateProject;
+
+
+public interface IBuffer : IBindable
+{
+    public void Allocate(int size);
+    public void Load(Array data, int size);
+    public void Load(IntPtr data, int size);
+    public void Update(Array data, int dataOffset, int offset, int size);
+    public void Update(IntPtr data, int dataOffset, int offset, int size);
+    public IntPtr Map(BufferAccess access);
+    public void Unmap();
+}
+
+public class IndexBuffer : IDisposable, IBuffer
+{
+    public BufferUsageHint Usage { get; }
+    public int Handle { get; }
+    public DrawElementsType Type { get; }
+    public int Count { get; set; }
+
+    public IndexBuffer(DrawElementsType type, int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw)
+    {
+        GL.CreateBuffers(1, out int handle);
+        Handle = handle;
+        Type = type;
+        Usage = usage;
+        Count = count;
+    }
+
+    public IndexBuffer(int size, DrawElementsType type, int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw) : this(type, count, usage)
+    {
+        Allocate(size);
+    }
+
+    public IndexBuffer(Array data, int size, DrawElementsType type, int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw) : this(type, count, usage)
+    {
+        Load(data, size);
+    }
+
+    public void Allocate(int size)
+    {
+        GL.NamedBufferData(Handle, size, IntPtr.Zero, Usage);
+    }
+
+    public void Load(Array data, int size)
+    {
+        var gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        Load(gcHandle.AddrOfPinnedObject(), size);
+        gcHandle.Free();
+    }
+
+    public void Load(IntPtr data, int size)
+    {
+        GL.NamedBufferData(Handle, size, data, Usage);
+    }
+
+    public void Update(Array data, int dataOffset, int offset, int size)
+    {
+        var gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        Update(gcHandle.AddrOfPinnedObject(), dataOffset, offset, size);
+        gcHandle.Free();
+    }
+
+    public void Update(IntPtr data, int dataOffset, int offset, int size)
+    {
+        GL.NamedBufferSubData(Handle, offset, size, data + dataOffset);
+    }
+
+    public IntPtr Map(BufferAccess access)
+    {
+        return GL.MapNamedBuffer(Handle, BufferAccess.ReadWrite);
+    }
+
+    public void Unmap()
+    {
+        GL.UnmapNamedBuffer(Handle);
+    }
+
+    public void Dispose()
+    {
+        GL.DeleteBuffer(Handle);
+        GC.SuppressFinalize(this);
+    }
+
+    public void Bind()
+    {
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, Handle);
+    }
+
+    public void Unbind()
+    {
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+    }
+}
+
+public class VertexBuffer : IBuffer, IDisposable
+{
+    public Attribute[] Attributes { get; }
+    public BufferUsageHint Usage { get; }
+    public int Handle { get; }
+    public int Count { get; set; }
+
+    public VertexBuffer(int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw, params Attribute[] attributes)
+    {
+        GL.CreateBuffers(1, out int handle);
+        Handle = handle;
+        Usage = usage;
+        Count = count;
+        Attributes = attributes;
+    }
+
+    public VertexBuffer(int size, int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw, params Attribute[] attributes) : this(count, usage, attributes)
+    {
+        Allocate(size);
+    }
+
+    public VertexBuffer(Array data, int size, int count = 0, BufferUsageHint usage = BufferUsageHint.StaticDraw, params Attribute[] attributes) : this(count, usage, attributes)
+    {
+        Load(data, size);
+    }
+
+    public void Allocate(int size)
+    {
+        GL.NamedBufferData(Handle, size, IntPtr.Zero, Usage);
+    }
+
+    public void CreateLayout(int vao, int index)
+    {
+        int stride = Attributes.Select(attrib => attrib.Size).Sum();
+        for (int i = 0, offset = 0; i < Attributes.Length; i++)
+        {
+            Attributes[i].Offset = offset;
+            Attributes[i].Stride = stride;
+            offset += Attributes[index].Size;
+        }
+        GL.VertexArrayVertexBuffer(vao, index, Handle, IntPtr.Zero, stride);
+        foreach (var attrib in Attributes)
+        {
+            GL.EnableVertexArrayAttrib(vao, attrib.Index);
+            GL.VertexArrayAttribBinding(vao, attrib.Index, index);
+            GL.VertexArrayAttribFormat(vao, attrib.Index, attrib.Count, attrib.Type, attrib.Normalized, attrib.Offset);
+        }
+    }
+
+    public void Load(Array data, int size)
+    {
+        var gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        Load(gcHandle.AddrOfPinnedObject(), size);
+        gcHandle.Free();
+    }
+
+    public void Load(IntPtr data, int size)
+    {
+        GL.NamedBufferData(Handle, size, data, Usage);
+    }
+
+    public void Update(Array data, int dataOffset, int offset, int size)
+    {
+        var gcHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+        Update(gcHandle.AddrOfPinnedObject(), dataOffset, offset, size);
+        gcHandle.Free();
+    }
+
+    public void Update(IntPtr data, int dataOffset, int offset, int size)
+    {
+        GL.NamedBufferSubData(Handle, offset, size, data + dataOffset);
+    }
+
+    public IntPtr Map(BufferAccess access)
+    {
+        return GL.MapNamedBuffer(Handle, access);
+    }
+
+    public void Unmap()
+    {
+        GL.UnmapNamedBuffer(Handle);
+    }
+
+    public void Dispose()
+    {
+        GL.DeleteBuffer(Handle);
+        GC.SuppressFinalize(this);
+    }
+
+    public void Bind()
+    {
+        GL.BindBuffer(BufferTarget.ArrayBuffer, Handle);
+    }
+
+    public void Unbind()
+    {
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+    }
+}
+
+public class Attribute
+{
+    public int Index { get; set; }
+    public int Count { get; set; }
+    public VertexAttribType Type { get; set; }
+    public bool Normalized { get; set; }
+    public int Stride { get; set; }
+    public int Offset { get; set; }
+    public int Size => Count * Sizes[Type];
+
+    private static Dictionary<VertexAttribType, int> Sizes { get; } =
+        new()
+        {
+            { VertexAttribType.Byte, 1 },
+            { VertexAttribType.UnsignedByte, 1 },
+            { VertexAttribType.Short, 2 },
+            { VertexAttribType.UnsignedShort, 2 },
+            { VertexAttribType.HalfFloat, 2 },
+            { VertexAttribType.Int, 4 },
+            { VertexAttribType.UnsignedInt, 4 },
+            { VertexAttribType.Float, 4 },
+            { VertexAttribType.Fixed, 4 },
+            { VertexAttribType.Int2101010Rev, 4 },
+            { VertexAttribType.UnsignedInt2101010Rev, 4 },
+            { VertexAttribType.UnsignedInt10F11F11FRev, 4 },
+            { VertexAttribType.Double, 8 }
+        };
+    public Attribute(int index, int count, VertexAttribType type = VertexAttribType.Float, bool normalized = false, int stride = 0, int offset = 0)
+    {
+        Index = index;
+        Count = count;
+        Type = type;
+        Normalized = normalized;
+        Stride = stride;
+        Offset = offset;
+    }
+}
