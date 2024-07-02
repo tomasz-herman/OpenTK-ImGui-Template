234e3e88cf2bda3be31d93af2627ffdebc3b6352
Tomasz Herman <tomasz.herman@protonmail.com>
2024-07-02 15:17:22 +0200

    space handling in shell script

--- untemplate.sh
+++ untemplate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo -n "Enter project name: "
-read PROJECT_NAME
+read -r PROJECT_NAME
 NAMESPACE="$(echo -e "${PROJECT_NAME}" | tr -d '[:space:]')"
 
-sed -i "s/TemplateProject/$NAMESPACE/g" OpenTK-ImGui-Template.sln
+sed -i "s/TemplateProject/${NAMESPACE}/g" OpenTK-ImGui-Template.sln
 
 for file in {**/**,**}/*.cs
 do
-    sed -i "s/TemplateProject/${NAMESPACE}/g" ${file}
-    sed -i "s/Project Title/${PROJECT_NAME}/g" ${file}
+    sed -i "s/TemplateProject/${NAMESPACE}/g" "${file}"
+    sed -i "s/Project Title/${PROJECT_NAME}/g" "${file}"
 done
 
-mv OpenTK-ImGui-Template.sln $NAMESPACE.sln
-mv TemplateProject/TemplateProject.csproj TemplateProject/$NAMESPACE.csproj
-mv TemplateProject $NAMESPACE
+mv OpenTK-ImGui-Template.sln "${NAMESPACE}".sln
+mv TemplateProject/TemplateProject.csproj TemplateProject/"${NAMESPACE}".csproj
+mv TemplateProject "${NAMESPACE}"
