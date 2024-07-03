0d53d0826a4ee84e935685657cf3b5d1ec066437
Tomasz Herman <tomasz.herman@protonmail.com>
2024-07-03 11:46:14 +0200

    ResourcesManager

---
+++ TemplateProject/ResourcesManager.cs
@@ -0,0 +1,26 @@
+namespace TemplateProject;
+
+public class ResourcesManager
+{
+    public static ResourcesManager Instance { get; } = new();
+
+    private Dictionary<string, Texture> Textures { get; } = new();
+    private Dictionary<string, Model> Models { get; } = new();
+
+    private ResourcesManager()
+    {
+        
+    }
+
+    public Texture GetTexture(string path)
+    {
+        if (Textures.TryGetValue(path, out var texture)) return texture;
+        return Textures[path] = new Texture(path);
+    }
+
+    public Model GetModel(string path)
+    {
+        if (Models.TryGetValue(path, out var model)) return model;
+        return Models[path] = ModelLoader.Load(path);
+    }
+}
